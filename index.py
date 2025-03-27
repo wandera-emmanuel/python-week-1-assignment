c546d04ed8bd31629dbeba87de799953b65d3afd
wandera-emmanuel <emmanuelbahati902@gmail.com>
2025-03-27 12:33:26 +0300

    python assignment

---
+++ index.py
@@ -0,0 +1,23 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operation = input("Enter an operation (+, -, *, /): ")
+
+# Perform the operation based on the user's input
+if operation == '+':
+    result = num1 + num2
+    print(f"{num1} + {num2} = {result}")
+elif operation == '-':
+    result = num1 - num2
+    print(f"{num1} - {num2} = {result}")
+elif operation == '*':
+    result = num1 * num2
+    print(f"{num1} * {num2} = {result}")
+elif operation == '/':
+    # Handle division by zero case
+    if num2 != 0:
+        result = num1 / num2
+        print(f"{num1} / {num2} = {result}")
+    else:
+        print("Error: Division by zero is not allowed!")
+else:
+    print("Error: Invalid operation entered!")
